69040d331bbf16c0caa5fd348a20a37ddd2ab928
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-07-18 00:47:18 +0200

    chore: add buffer/stream polyfill aliases in metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+// Polyfill Node.js core modules used by starknet.js
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  buffer: path.resolve(__dirname, 'node_modules/buffer'),
+  stream: require.resolve('stream-browserify'),
+};
 
+module.exports = config;
